5e5a8d7468aeb1bcc8e56c43fa83b7e248c15d8c
Bunz <66331265+66Bunz@users.noreply.github.com>
2021-06-08 14:05:29 +0200

    2.2 update
    Added localization (added English, Italian, Polish [thanks Jacob_Officjal_PL]), Code simplification, Bug fixes

--- Mondaine Stop2Go/@Resources/SettingsLog/ResetSettings.bat
+++ Mondaine Stop2Go/@Resources/SettingsLog/ResetSettings.bat
@@ -5,14 +5,14 @@ title Resetting Skin Settings...
 
 echo.
 echo * Deleting backup settings...
-del SettingsLogBackup.ini
+del SettingsLogBackup.inc
 echo * Backing up current settings...
-ren SettingsLog.ini SettingsLogBackup.ini
+ren SettingsLog.inc SettingsLogBackup.inc
 echo * Loading default settings...
 md temp
-copy /Y SettingsLogDefaults.ini .\temp
-ren .\temp\SettingsLogDefaults.ini SettingsLog.ini
-copy /Y .\temp\SettingsLog.ini .\
+copy /Y SettingsLogDefaults.inc .\temp
+ren .\temp\SettingsLogDefaults.ini SettingsLog.inc
+copy /Y .\temp\SettingsLog.inc .\
 rd /S /Q temp
 goto end
 
